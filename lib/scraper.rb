bb2aaeeaa33c542a2da0d377c762405d76752f6f
madifana <madifana@yahoo.com>
2017-02-02 09:28:38 -0800

    troubleshooting

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,14 +2,19 @@ class StockScreener::Scraper
 	def scrape_page	
 		Nokogiri::HTML(open("http://www.finviz.com/screener.ashx?v=111&f=fa_netmargin_pos,sh_avgvol_o2000,ta_candlestick_d&ft=4"))
 	end
+	#screener-content > table > tbody > tr:nth-child(4) > td > table
+
+	#screener-content > table > tbody > tr:nth-child(4) > td > table > tbody > tr
 
 	def scrape_stocks
-		self.scrape_page.css("tbody[3] tr")
+		sleep 10
+		self.scrape_page.css("#screener-content > table > tbody > tr:nth-child(4) > td > table > tbody > tr")
+
 	end
 
 	def make_stocks
 		scrape_stocks.each do |r|
-			self.new_from_index_page(r)
+			StockScreener::Screener.new_from_screener(r)
 		end
 	end
 
--- lib/screener.rb
+++ lib/screener.rb
@@ -2,9 +2,22 @@ class StockScreener::Screener
 
 attr_accessor :ticker, :company, :sector, :price, :url
 
+	def self.new_from_screener(r)
+		self.new(
+			r.css(a"screener-link-primary").text,
+			"http://www.finviz.com/screener.ashx?v=111&f=fa_netmargin_pos,sh_avgvol_o2000,ta_candlestick_d&ft=4",
+			r.css(td[3]).text,
+			r.css(td[4]).text,
+			r.css(td[9]).text
+			)
+	end
+
 	def initialize(ticker=nil, url=nil)
 		@ticker = ticker
 		@url = url
+		@company = company
+		@sector = sector
+		@price = price
 	end
 
 	def self.all
@@ -41,12 +54,7 @@ attr_accessor :ticker, :company, :sector, :price, :url
 		tickers.collect{|e| new(e.text.strip, "http://finviz.com#{e.attr("href").split("?").first.strip}")}
 	end
 
-	
-
 	def doc
 		@doc = Nokogiri::HTML(open("http://www.finviz.com/screener.ashx?v=111&f=fa_netmargin_pos,sh_avgvol_o2000,ta_candlestick_d&ft=4"))
 	end
-
-	
-
 end
